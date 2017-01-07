71c58ca2b1f9e7ca40ec0cf055596ceffb3b208b
yanil3500 <yanil3500@gmail.com>
2017-01-06 16:11:10 -0800

    multiply-array-function works

--- app.js
+++ app.js
@@ -113,10 +113,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(testArray) { //eslint-disable-line
-
+  var firstProduct = multiply(testArray[0], testArray[1]);
+  console.log(firstProduct[0]);
+  var finalProduct = multiply(firstProduct[0], testArray[2]);
+  console.log(finalProduct[0]);
+  finalProduct[1] = 'The numbers ' + testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' have a product of ' + finalProduct[0] + '.';
+  return [finalProduct[0], finalProduct[1]];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray([2,3,4]);
+testMultiplyArray([2,3,4]);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
